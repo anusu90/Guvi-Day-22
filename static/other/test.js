4fe4175d035b309a1acf2cd1e0520132adb56d78
anunay sinha <anusu90@gmail.com>
2020-12-09 16:48:33 +0530

    test

---
+++ static/other/test.js
@@ -0,0 +1,5 @@
+function funcDef(a, b) {
+    return a + b;
+}
+console.log(funcDef('hiiiii', 5));
+console.log(funcDef(8, 9));
---
+++ static/other/test.ts
@@ -0,0 +1,7 @@
+function funcDef(a:any , b:any){
+    return a + b
+}
+
+
+console.log(funcDef('hiiiii', 5))
+console.log(funcDef(8,9))
